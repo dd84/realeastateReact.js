6bd6dd4514c493811ff8857f9a36c9fb37f72436
abc <abc@example.com>
2018-09-01 05:42:40 +0100

    withCard

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react';
 
 import './App.css';
 import Header from './shared/Header'
+import RentalCard from './components/rental/RentalCard'
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div classNameName="App">
           <Header/>
+          <h1 class='page-title'>Your Home All Around the World</h1>
+          <RentalCard/>
+          <RentalCard/>
+          <RentalCard/>
+
+
       </div>
     );
   }
---
+++ src/components/rental/RentalCard.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+
+export default class RentalCard extends Component {
+  render() {
+    return (
+      <div>
+      <div className='container'>
+      <section id='rentalListing'>
+        <div class='row'>
+          <div class='col-md-3 col-xs-6'>
+            <div class='card bwm-card'>
+              <img class='card-img-top' src='http://via.placeholder.com/350x250' alt=''></img>
+              <div class='card-block'>
+                <h6 class='card-subtitle'>Whole Apartment &#183; New York</h6>
+                <h4 class='card-title'>Some nice apartment</h4>
+                <p class='card-text'>$240 per Night &#183; Free Cancelation</p>
+                <a href='' class='card-link'>More Info</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+    
+      </div>
+    )
+  }
+}
